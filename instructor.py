c12036034e2454700402a13854b6513984be78f1
Davis Lindell <daviselindell@gmail.com>
2020-07-20 13:00:03 -0500

    refactored for inheiratance

--- instructor.py
+++ instructor.py
@@ -1,11 +1,10 @@
-class Instructor:
+from nssPerson import NSSPerson
+
+class Instructor(NSSPerson):
 
     def __init__(self, first_name, last_name, slack_handle, specialty, cohort_name):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.slack_handle = slack_handle
+        super().__init__(first_name, last_name, slack_handle, cohort_name)
         self.specialty = specialty
-        self.cohort_name = cohort_name
 
     def assign_exercise(self, student, exercise):
         student.add_exercise(exercise)
--- student.py
+++ student.py
@@ -1,10 +1,9 @@
-class Student:
+from nssPerson import NSSPerson
+
+class Student(NSSPerson):
     
     def __init__(self, first_name, last_name, slack_handle, cohort_name):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.slack_handle = slack_handle
-        self.cohort_name = cohort_name
+        super().__init__(first_name, last_name, slack_handle, cohort_name)
         self.exercises_collection = []
 
     def add_exercise(self, exercise):
